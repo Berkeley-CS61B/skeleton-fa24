479395a8302342631fff496336aa39dc8d4bf45e
peyrin <peyrin@berkeley.edu>
2024-11-18 14:33:28 -0800

    [proj3] add comments to GameLoopDemo

--- proj3/src/demo/GameLoopDemo.java
+++ proj3/src/demo/GameLoopDemo.java
@@ -5,11 +5,19 @@ import tileengine.TERenderer;
 import tileengine.TETile;
 import tileengine.Tileset;
 
+/**
+ * This demo shows how to build an interactive game.
+ * In this demo, we create a world with 5 squares.
+ * Pressing keys 1 2 3 4 5 causes the corresponding square to change between FLOOR and TILE.
+ * Pressing key q causes the game to quit.
+ */
 public class GameLoopDemo {
     private static final int WIDTH = 5;
     private static final int HEIGHT = 1;
 
-    /* If the tile at position (x,y) is FLOOR, change it to WALL.
+    /**
+     * Helper function.
+     * If the tile at position (x,y) is FLOOR, change it to WALL.
      * If the tile at position (x,y) is WALL, change it to FLOOR.
      */
     public static void toggle(TETile[][] world, int x, int y) {
@@ -21,12 +29,13 @@ public class GameLoopDemo {
     }
 
     public static void main(String[] args) {
-        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        // Initialize the tile rendering engine with a window of size WIDTH x HEIGHT.
         // This WIDTH and HEIGHT is very small!
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
 
-        // initialize tiles
+        // Fill in the tile grid with FLOOR tiles.
+        // If you don't fill in the grid, you'll get a NullPointerException!
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
@@ -34,12 +43,24 @@ public class GameLoopDemo {
             }
         }
 
-        char c;
+        char c; // Variable for saving the most recent character typed by the user.
+
+        // This outer infinite-loop allows the game to continue indefinitely, until the user quits.
         while (true) {
+
+            // hasNextKeyTyped checks if the user has typed a key that we haven't processed.
+            // This loop runs until all unprocessed keys are processed.
+            // If there are no unprocessed keys, we go back to the outer infinite loop to wait for the next key.
             while (StdDraw.hasNextKeyTyped()) {
+
+                // nextKeyTyped returns the next key to process.
+                // Always check hasNextKeyTyped before calling nextKeyTyped.
+                // If you call nextKeyTyped and there's no key to process, code will crash!
                 c = StdDraw.nextKeyTyped();
+
                 c = Character.toLowerCase(c);
 
+                // Switch statements can be useful to replace long if-else statements!
                 switch (c) {
                     case '1':
                         toggle(world, 0, 0);
@@ -57,7 +78,7 @@ public class GameLoopDemo {
                         toggle(world, 4, 0);
                         break;
                     case 'q':
-                        System.exit(0);
+                        System.exit(0); // Closes the game window and quits the game.
                         break;
                     default:
                         break;
@@ -65,7 +86,8 @@ public class GameLoopDemo {
 
             }
 
-            // draws the world to the screen
+            // Draws the world to the screen.
+            // This is in the while(true) loop, because we want to frequently re-render the world.
             ter.renderFrame(world);
         }
     }
